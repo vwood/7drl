aac879ce5593f3fbc73e8154a145cc9d3a87abe2
V Wood <vwood.org@gmail.com>
2012-03-13 12:41:22 +1300

    Tile definition file

---
+++ tiles.py
@@ -0,0 +1,32 @@
+#
+# Tiles
+#
+
+images = ["floor1.png",
+          "floor2.png",
+          "floor3.png",
+          "floor4.png",
+          "floor5.png",
+          "wall1.png",
+          "wall2.png",
+          "wall3.png",
+          "wall4.png",
+          "wall5.png",
+          "door.png",
+          "opendoor.png",
+          "stairs.png",
+          "water.png",
+          "lava.png",
+          "player.png",
+          "catdog.png",
+          "goblin.png",
+          "dwarf.png",
+          "janitor.png",
+          "shopkeeper.png",
+          "wizard.png",
+          "bard.png",
+          "cthulhu.png",
+          "wand.png"]
+
+floors = [0, 1, 2, 3, 4]
+walls = [5, 6, 7, 8, 9]
