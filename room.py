e85924bb206cd9d48bb1707605582ee127a152f5
V Wood <vwood.org@gmail.com>
2012-03-13 12:42:05 +1300

    Generate random rooms

--- room.py
+++ room.py
@@ -8,7 +8,7 @@ from random import randint
 from google.appengine.ext import db 
 
 from base import *
-
+from tiles import *
 
 class Room(db.Model):
     """Models a location (location ~= page)."""
@@ -48,3 +48,51 @@ class RoomEditor(BaseHandler):
         all_room_keys = Room.all(keys_only=True).run()
         self.render_template('static/html/room_editor.html', {'room_keys' : all_room_keys})
     get = require_admin(get)
+
+class RandomRoom(BaseHandler):
+    def get(self):
+        tiles = generate_room()
+        self.render_template('static/html/simple_room.html', {'tiles' : tiles})
+    get = require_admin(get)
+    
+def generate_room():
+    width, height = 7, 5
+
+    floor = floors[randint(0, len(floor)-1)]
+    wall = walls[randint(0, len(floor)-1)]
+
+    result = [[floor for _ in width] for _ in height]
+
+    for x in width:
+        result[x][0] = wall
+        result[x][height - 1] = wall
+
+    for y in height:
+        result[0][y] = wall
+        result[width - 1][y] = wall
+
+    if randint(0, 4) == 0:
+        result[1][1] = wall
+        result[2][1] = wall
+
+    if randint(0, 4) == 0:
+        result[4][1] = wall
+        result[5][1] = wall
+
+    if randint(0, 4) == 0:
+        result[1][3] = wall
+        result[2][3] = wall
+
+    if randint(0, 4) == 0:
+        result[4][3] = wall
+        result[5][3] = wall
+
+    return result
+
+def tile_string_to_arrays(tile_string):
+    return [[int(i) for i in row.split(",")]
+            for row in tile_string.split("|")]
+
+def tile_arrays_to_string(tile_arrays):
+    return "|".join([",".join([str(i) for i in row]))
+                    for row in tile_arrays])
