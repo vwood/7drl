40236f2f9c216645f2d746c3de70206b728c7142
V Wood <vwood.org@gmail.com>
2012-03-13 17:17:23 +1300

    Players can move and be generated

--- player.py
+++ player.py
@@ -1,3 +1,4 @@
+from google.appengine.api import users
 from google.appengine.ext import db
 
 from base import *
@@ -10,22 +11,36 @@ class Player(db.Model):
     location = db.ReferenceProperty(Room)
     is_alive = db.BooleanProperty()
     
-def get_player(user):
+def get_player(user = None):
     "Gets the player model of a current user."
+    if not user:
+        user = users.get_current_user()
     return Player.gql("WHERE user = :1", user).get()
     
 class Move(BaseHandler):
     def post(self):
-        pass
+        player = get_player()
+        try:
+            target_exit = int(self.request.get('exit'))
+            exits = player.location.exits
+            if target_exit >= len(exits):
+                self.error(400)
+        except:
+            self.error(400)
+        player.location = exits[target_exit]
+        player.put()
     post = require_player(post)
 
 class CreatePlayer(BaseHandler):
     def post(self):
         player = Player()
+        player.user = users.get_current_user()
         player.is_alive = True
         player.location = World.start
         player.put()
-
+    post = require_user(post)
+    
 class CharacterGeneration(BaseHandler):
     def get(self):
-        pass
+        self.render_template('static/html/character_generation.html')
+    get = require_user(get)
