49dc05b2985c8293c943631235de604b20896a70
V Wood <vwood.org@gmail.com>
2012-03-14 17:14:16 +1300

    Use items

--- item.py
+++ item.py
@@ -2,12 +2,32 @@ from google.appengine.ext import db
 
 from base import *
 from item_type import *
+from player import *
 
 class Item(db.Model):
     """Models a particular instance of some strange
     artefact of mystery and power."""
     item_type = db.IntegerProperty()
     location = db.ReferenceProperty(None)
+    
+class Use(BaseHandler):
+    """Handles the usage of items"""
+    def post(self):
+        # check player has item
+        player = get_player()
+        item = Item.get(self.request.get_by_key_name('item'))
+        if item.location != player:
+            self.error(400)
+        elif item.item_type == 0:
+            # Wand of Digg
+            self.redirect("http://digg.com")
+            # Book of Faces
+            # Tube of You
+            # Spellapedia (learn a random spell?)
+        else:
+            pass
+    post = require_player(post)
+
     
 def drop(item):
     if item.location.kind() == "Player":
