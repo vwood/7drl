fdc6c4bd215f7dc3f4ed1addf8ae232132009186
V Wood <vwood.org@gmail.com>
2012-03-14 22:30:00 +1300

    Make monsters move.

--- update.py
+++ update.py
@@ -5,6 +5,10 @@
 from player import *
 from monster import *
 
+monsters = Monster.gql("WHERE is_alive = TRUE").run()
+for monster in monsters:
+    move(monster)
+
 # Bring out your dead!
 dead = Player.gql("WHERE is_alive = FALSE").run()
 for dodo in dead:
