239b1bc91f567a4db9e256bdba136458fc1005aa
V Wood <vwood.org@gmail.com>
2012-03-14 22:28:54 +1300

    Add more to monsters.

--- monster.py
+++ monster.py
@@ -1,16 +1,28 @@
+from random import randint
+
 from google.appengine.ext import db
 
 from base import *
 from room import *
 
+
 class Monster(db.Model):
     """Models some hideous creature."""
     name = db.StringProperty()
     location = db.ReferenceProperty(Room)
+    health = db.IntegerProperty()
     is_alive = db.BooleanProperty()
     
-def move():
-    pass
+def move(monster):
+    # is a player here?
+    # dispatch based on what kind of monster I am...
+    if randint(0,1) == 0:
+        return
+    exits = monster.location.exits
+    target = randint(0, len(exits)-1)
+    if exits[target] is not None:
+        monster.location = exits[target]
+        monster.put()
 
 def attack():
     pass
