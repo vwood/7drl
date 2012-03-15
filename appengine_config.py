342426315d067f1ab136d3265b47dd1ef98f6d7a
V Wood <vwood.org@gmail.com>
2012-03-15 17:16:08 +1300

    Attempt to add custom filters to django (which is woefully incomplete)

--- appengine_config.py
+++ appengine_config.py
@@ -1,2 +1,15 @@
 from google.appengine.dist import use_library
 use_library('django', '1.2')
+
+from google.appengine.ext import webapp
+register = webapp.template.create_template_register()
+
+def divide(value, arg):
+    return int(value) / int(arg)
+
+register.filter('divide', divide)
+
+def mod(value, arg):
+    return int(value) % int(arg)
+
+register.filter('mod', mod)
