ac39f01b761da268cc8c88379fdeda95554cf0dd
V Wood <vwood.org@gmail.com>
2012-03-16 12:14:07 +1300

    Let players move again after a heartbeat update

--- update.py
+++ update.py
@@ -9,6 +9,11 @@ monsters = Monster.gql("WHERE is_alive = TRUE").run()
 for monster in monsters:
     move(monster)
 
+players = Player.gql("WHERE has_moved = TRUE AND is_alive = TRUE").run()
+for player in players:
+    player.has_moved = False
+    player.put()
+
 # Bring out your dead!
 dead = Player.gql("WHERE is_alive = FALSE").run()
 for dodo in dead:
