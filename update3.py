8261fb864084801eb051608cc2739b189092721a
V Wood <vwood.org@gmail.com>
2012-03-18 04:51:34 +1300

    add robots.txt

---
+++ update3.py
@@ -0,0 +1,10 @@
+#
+# Update
+#
+from google.appengine.api import taskqueue
+
+tick.add(queue_name='tick', url="/update")
+tick.add(queue_name='tick', url="/update")
+tick.add(queue_name='tick', url="/update")
+tick.add(queue_name='tick', url="/update")
+tick.add(queue_name='tick', url="/update")
