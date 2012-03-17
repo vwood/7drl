323e8ce7a0fdaa64441c611f12c7e5e826562a2c
V Wood <vwood.org@gmail.com>
2012-03-18 03:46:32 +1300

    Use memcache for player_count queries.

--- status.py
+++ status.py
@@ -1,5 +1,10 @@
+from google.appengine.api import memcache 
+
 import player
 
-player_count = player.Player.all().count()
+player_count = memcache.get("player_count")
+if player_count is None:
+    player_count = player.Player.all().count()
+    memcache.set("player_count", player_count, 120)
 
 print player_count
